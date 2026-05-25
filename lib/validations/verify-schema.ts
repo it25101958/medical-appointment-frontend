b0291e535e8f9b2260d4d4eef90823f732621dbe
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2026-05-25 21:02:57 +0530

    feat(validations): add verification schema for code validation

---
+++ lib/validations/verify-schema.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const verifySchema = z.object({
+  code: z
+    .string()
+    .min(6, "Verification code must be 6 digits.")
+    .max(6, "Verification code must be 6 digits."),
+});
