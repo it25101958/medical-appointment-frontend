8193551d4baaa025d37e419f4c02f0f1b34dc2af
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2026-07-08 18:32:20 +0530

    refactor(room-schedule): export formatDayOfWeek from room-schedule-formatters

--- src/features/room-schedule/index.ts
+++ src/features/room-schedule/index.ts
@@ -5,4 +5,5 @@ export type {
 } from "./types/room-schedule.types";
 
 export { roomScheduleApi } from "./api/room-schedule.api";
+export { formatDayOfWeek } from "./utils/room-schedule-formatters";
 // Room APIs moved to `features/room`
